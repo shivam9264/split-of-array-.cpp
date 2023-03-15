559fec4d577eb3e51090a3af7c46bcc266bb2e8a
shivam9264 <127966011+shivam9264@users.noreply.github.com>
2023-03-15 20:28:10 +0530

    Create split of array.cpp

---
+++ split of array.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "solution.h"
+using namespace std;
+int main() {
+    int size;
+    cin>>size;
+    int *input=new int[1+size];
+    for(int i=0;i<size;i++)
+        cin>>input[i];
+    if(splitArray(input, size)) {
+        cout << "true" << endl;
+    }
+    else {
+        cout << "false" << endl;
+    }
+    return 0;
+}
+*/
+
+bool spliter(int arr[], int n, int start, int ls, int rs)
+{
+
+    if (start == n)
+        return ls == rs;
+    if (arr[start] % 5 == 0)
+        ls += arr[start];
+    else if (arr[start] % 3 == 0)
+        rs += arr[start];
+    else
+        return spliter(arr, n, start + 1, ls + arr[start], rs) || spliter(arr, n, start + 1, ls, rs + arr[start]);
+    return spliter(arr, n, start + 1, ls, rs);
+}
+bool splitArray(int *input, int size)
+{
+    /* Don't write main().
+     * Don't read input, it is passed as function argument.
+     * Return output and don't print it.
+     * Taking input and printing output is handled automatically.
+     */
+    return spliter(input, size, 0, 0, 0);
+}
